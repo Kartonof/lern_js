d1267593740ba9e62dabb7bb0ceb6baf5340f51a
Kartonof <gipsumkartonof@gmail.com>
2021-10-16 12:32:14 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//const result = confirm("Are you here?");
+//console.log(result);
+
+//const answer = prompt("Вам есть 18?", "18");
+//console.log(typeof(answer));
+
+const answers = [];
+
+//answers[0] = prompt('Как Ваше имя?', '');
+//answers[1] = prompt('Какая Ваша фамилия?', '');
+//answers[2] = prompt('Сколько Вам лет?', '');
+
+console.log(typeof(answers));
